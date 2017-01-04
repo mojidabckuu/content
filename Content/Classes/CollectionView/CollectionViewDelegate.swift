e3089a7279a74f18bbed7bf06073df68948cb81b
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2017-01-04 12:15:52 +0400

    Implement collection insert/reload/delete

--- Content/Classes/CollectionView/CollectionViewDelegate.swift
+++ Content/Classes/CollectionView/CollectionViewDelegate.swift
@@ -14,6 +14,7 @@ open class CollectionDelegate<Model: Equatable, View: ViewDelegate, Cell: Conten
  
     var collectionView: UICollectionView { return self.content.view as! UICollectionView }
     
+    //MARK: - Lifecycle
     public override init() {
         super.init()
     }
@@ -21,33 +22,49 @@ open class CollectionDelegate<Model: Equatable, View: ViewDelegate, Cell: Conten
     override init(content: Content<Model, View, Cell>) {
         super.init(content: content)
     }
- 
-    // Insert
     
+    // Insert
     override open func insert(_ models: [Model], index: Int) {
+        self.content.items.insert(contentsOf: models, at: index)
         let indexPaths = self.indexPaths(models)
         let collectionView = self.collectionView
         self.collectionView.performBatchUpdates({ 
             collectionView.insertItems(at: indexPaths)
         }, completion: nil)
-        
     }
-        
-    override open func indexPath(_ cell: Cell) -> IndexPath? {
-        if let collectionViewCell = cell as? UICollectionViewCell {
-            return self.collectionView.indexPath(for: collectionViewCell)
+    
+    //Delete
+    open override func delete(_ models: [Model]) {
+        var indexes = self.content.items
+            .flatMap { self.content.items.index(of: $0) }
+            .map {IndexPath(row: $0, section: 0)}
+        let collectionView = self.collectionView
+        let content = self.content
+        self.collectionView.performBatchUpdates({
+            indexes.forEach { content?.items.remove(at: $0.row) }
+            collectionView.deleteItems(at: indexes)
+        }, completion: nil)
+    }
+    
+    //Reload
+    open override func reload(_ models: [Model], animated: Bool) {
+        let indexes = self.indexPaths(models)
+        let collectionView = self.collectionView
+        if animated {
+            collectionView.performBatchUpdates({
+                collectionView.reloadItems(at: indexes)
+            }, completion: nil)
+        } else {
+            collectionView.reloadItems(at: indexes)
         }
-        return nil
     }
     
     // Registration
-    
     override open func registerCell(_ reuseIdentifier: String, nib: UINib) {
         self.collectionView.register(nib, forCellWithReuseIdentifier: reuseIdentifier)
     }
     
-    // UICollectionView delegate
-    
+    //MARK: - UICollectionView delegate
     open func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let cell = collectionView.cellForItem(at: indexPath) as! Cell
         self.content.actions.onSelect?(self.content, self.content.items[indexPath.row], cell)
@@ -61,8 +78,7 @@ open class CollectionDelegate<Model: Equatable, View: ViewDelegate, Cell: Conten
         self.content.actions.onDeselect?(self.content, self.content.items[indexPath.row], cell)
     }
     
-    // UICollectionView data
-    
+    //MARK: - UICollectionView data
     open func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.content.items.count
     }
@@ -99,5 +115,13 @@ open class CollectionDelegate<Model: Equatable, View: ViewDelegate, Cell: Conten
     
     public func scrollViewDidScroll(_ scrollView: UIScrollView) {
         self.content.scrollCallbacks.onDidScroll?(self.content)
+    }
+    
+    //MARK: - Utils
+    override open func indexPath(_ cell: Cell) -> IndexPath? {
+        if let collectionViewCell = cell as? UICollectionViewCell {
+            return self.collectionView.indexPath(for: collectionViewCell)
+        }
+        return nil
     }
 }
