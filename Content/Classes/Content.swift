602c2c25a0aef3643db4d7a0a6f0fc76611aff0e
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2017-10-28 03:12:31 +0300

    Add last/first + index of

--- Content/Classes/Content.swift
+++ Content/Classes/Content.swift
@@ -25,6 +25,13 @@ open class Content<Model: Equatable, View: ViewDelegate, Cell: ContentCell>: Act
     open var count: Int { return adapter.count }
     open var isEmpty: Bool { return adapter.isEmpty }
     
+    func index(of model: Model) -> Int? {
+        return adapter.index(of: model)
+    }
+    
+    var first: Model? { return adapter.first }
+    var last: Model? { return adapter.last }
+    
     open subscript (position: Int) -> Model {
         get { return self.adapter[position] }
         set { self.replace(newValue, animated: true) }
